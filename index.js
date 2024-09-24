1e8d07c2e3ecb3f71394838e26b156a9dfd39e36
Ashley Natasha <ashleyotsiula@gmail.com>
2024-09-24 15:39:01 +0300

    control flow

--- index.js
+++ index.js
@@ -1,11 +1,39 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(isfeet){
+  if(isfeet<=400 ) {
+    return "This one is on me!"
+  }
+
+  else if(isfeet>=400 && isfeet<=2000){
+    return "That will be twenty bucks."
+  }
+
+  else if(isfeet>=2001 && isfeet<=2499){
+    return "I will gladly take your thirty bucks."
+  }
+
+  else if(isfeet>=2500){
+    return "No can do."
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  if(city === "NYC") {
+    return "Ok, sounds good."
+  }
+
+  else {
+    return "No go."
+  }
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  if(tip === "generous") {
+    return "Thank you so much."
+  }
+
+  else if(tip==="not as generous") {
+    return "Thank you."
+  }
+
+  else {return "Bye."}
 }
